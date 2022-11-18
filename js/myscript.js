35af5ad7f0758b100174eba9496ca47c6cf133e5
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-18 16:43:35 +0100

    fix final const and finish

--- js/myscript.js
+++ js/myscript.js
@@ -17,5 +17,12 @@ console.log(popup);
 let favColor = prompt("Qual'è il tuo colore preferito? e perché proprio il Blu");
 console.log(favColor);
 
+// creo la variabile che racchiuder le 3 risposte precedenti
+
+const password = userName + lastName + favColor + "22";
+console.log(password);
+
+document.getElementById("passwordtext").innerHTML = password;
+
 popup = ("Ben fatto! Ora il tuo Account è correttamente registrato, puoi procedere col tuo ordine");
 console.log(popup);
