0a70987238011772310fd8568061269edefdd621
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-18 15:51:19 +0100

    three questions user

---
+++ js/myscript.js
@@ -0,0 +1,21 @@
+const userName = prompt("Ciao! Come ti chiami?")
+
+
+console.log(userName)
+
+let popup = ("Bravissimo! Andiamo avanti")
+console.log(popup)
+
+
+const lastName = prompt("Dimmi ora il tuo cognome")
+console.log(lastName)
+
+
+popup = (" Manca poco! Solo un'ultima domanda")
+console.log(popup)
+
+let favColor = prompt("Qual'è il tuo colore preferito? e perché proprio il Blu")
+console.log(favColor)
+
+popup = ("Ben fatto! Ora il tuo Account è correttamente registrato, puoi procedere col tuo ordine")
+console.log(popup)
